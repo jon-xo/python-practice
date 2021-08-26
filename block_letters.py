9ff1832cb6026f618cc3d896415ca6478dc656ee
Jon Newton <jonn@hey.com>
2021-08-26 11:04:04 -0400

    Created block_letters.py to pratice multi-line printing

---
+++ block_letters.py
@@ -0,0 +1,34 @@
+# Jon Newton
+# Fun Fact: I ride motorcycles
+
+first_inital = """
+JJJJJ   
+  J     
+  J     
+  J      
+J J     
+J J     
+ JJ     
+"""
+
+last_inital = """
+N   N
+NN  N
+N N N
+N  NN
+N   N
+N   N
+N   N 
+"""
+
+full_initals = """
+JJJJJ N   N  
+  J   NN  N  
+  J   N N N  
+  J   N  NN   
+J J   N   N  
+J J   N   N  
+ JJ   N   N   
+"""
+
+print(full_initals)
