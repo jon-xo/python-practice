3f7bd4862fbb4fdeb08b4ea0af3a45d3a376b6ea
Jon Newton <jonn@hey.com>
2021-08-30 16:29:31 -0400

    Created space.py for additional conditional practice

---
+++ space.py
@@ -0,0 +1,27 @@
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+weight = 185
+planet = 3
+
+if planet == 1:
+    # Venus
+    print(weight * 0.91)
+elif planet == 2:
+    # Mars
+    print(weight * .38)
+elif planet == 3:
+    # Jupiter
+    print(weight * 2.34)
+elif planet == 4:
+    # Saturn
+    print(weight * 1.06)
+elif planet == 5:
+    # Uranus
+    print(weight * 0.92)
+elif planet == 6:
+    # Neptune
+    print(weight * 1.19)
+    
