c52f9868da6e91200e926cf4a59d5947173a2e3f
Jon Newton <jonn@hey.com>
2021-08-30 16:32:54 -0400

    Created magic8.py with script description.

---
+++ magic8.py
@@ -0,0 +1,12 @@
+# magic8.py answers yes / no questions with 
+# one of 9 possible answers
+#
+# - "Yes - definitely."
+# - "It is decidedly so."
+# - "Without a dobut."
+# - "Reply hazy, try again."
+# - "Better not tell you now."
+# - "My sources say no."
+# - "Outlook not so good."
+# - "Very dobutful."
+
