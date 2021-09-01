4af5e9d974f03e7e7cd5b4028957301bd753cd40
Jon Newton <jonn@hey.com>
2021-09-01 13:52:06 -0400

    Updated shipping.py with conditional statement to calculate ground shipping costs

--- shipping.py
+++ shipping.py
@@ -7,6 +7,24 @@
 
 ## Prompt user with input for package weight
 
+weight = 11
+
+# Ground Shipping Cost
+ground_flat = 20
+
+if weight <= 2:
+    weight = (weight * 1.50) + ground_flat
+    print(weight)
+elif weight > 2 and weight <= 6:
+    weight = (weight * 3.00) + ground_flat
+    print(weight)
+elif weight > 6 and weight <= 10:
+    weight = (weight * 4.00) + ground_flat
+    print(weight)
+else:
+    weight = (weight * 4.75) + ground_flat
+    print(weight)
+
 ## Determine which shipping method is the least expensive
 
 ## Provide total shipping cost to user
