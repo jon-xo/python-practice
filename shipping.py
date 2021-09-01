7343d29c39e11a13945a0a0d1c6cfe7b86ece683
Jon Newton <jonn@hey.com>
2021-09-01 14:31:30 -0400

    Updated script to prompt user for input and convert returned data to float, added descriptions to printed shipping costs

--- shipping.py
+++ shipping.py
@@ -7,27 +7,49 @@
 
 ## Prompt user with input for package weight
 
-weight = 8.4
+weight = float(input("Enter package weight (lbs): "))
 
 # Ground Shipping Cost
 ground_flat = 20
 
 if weight <= 2:
-    weight = (weight * 1.50) + ground_flat
-    print(weight)
+    ground_cost = (weight * 1.50) + ground_flat
+    # print(ground_cost)
 elif weight > 2 and weight <= 6:
-    weight = (weight * 3.00) + ground_flat
-    print(weight)
+    ground_cost = (weight * 3.00) + ground_flat
+    # print(ground_cost)
 elif weight > 6 and weight <= 10:
-    weight = (weight * 4.00) + ground_flat
-    print(weight)
+    ground_cost = (weight * 4.00) + ground_flat
+    # print(ground_cost)
 else:
-    weight = (weight * 4.75) + ground_flat
-    print(weight)
+    ground_cost = (weight * 4.75) + ground_flat
+    # print(ground_cost)
 
-# Premium Ground Cost
-premium_ground = 125.00
-print("Premium Ground Cost: " + str(premium_ground))
+print()
+# Ground Total Cost
+print("Standard Ground Shipping: $" + str(ground_cost))
+
+# Premium Ground Total Cost
+ground_premium_cost = 125.00
+print("Premium Ground: $" + str(ground_premium_cost))
+
+# Drone Ship
+
+if weight <= 2:
+    drone_cost = (weight * 4.50)
+    # print(drone_cost)
+elif weight > 2 and weight <= 6:
+    drone_cost = (weight * 9.00)
+    # print(drone_cost)
+elif weight > 6 and weight <= 10:
+    drone_cost = (weight * 12.00)
+    # print(drone_cost)
+else:
+    drone_cost = (weight * 14.25)
+    # print(drone_cost)
+
+# Ground Total Cost
+print("Drone Shipping: $" + str(drone_cost))
 
 ## Determine which shipping method is the least expensive
 
