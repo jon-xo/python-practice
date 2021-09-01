49eaa8ed19d46ee9ba767b619e415300b6f304d6
Jon Newton <jonn@hey.com>
2021-09-01 13:27:58 -0400

    Created shipping.py with comments to define script function

---
+++ shipping.py
@@ -0,0 +1,12 @@
+# shipping.py
+## script accepts package weight and determines the least expensive
+## choice from the list of shipping options:
+## - Ground Shipping: flat charge + weight based rate
+## - Ground Shipping Premium: higher single flat charge 
+## - Drone Shipping: high weight based rate charge
+
+## Prompt user with input for package weight
+
+## Determine which shipping method is the least expensive
+
+## Provide total shipping cost to user
