c929124665aa0625f56810dbdbaba63360f85e10
Jon Newton <jonn@hey.com>
2021-09-02 10:28:22 -0400

    Added additional pratice section to work with negative indexes

--- list_selection.py
+++ list_selection.py
@@ -1,6 +1,8 @@
 # list_selection.py
 ## A negative index can be used to select the last element in a list
 
+## Pratice 1
+
 shopping_list = ["eggs", "butter", "milk", "cucumbers", "juice", "cereal"]
 
 last_element = shopping_list[-1]
@@ -9,3 +11,11 @@ index5_element = shopping_list[5]
 
 print(last_element)
 print(index5_element)
+
+## Pratice 2
+
+garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+garden_waitlist[1] = "Calla"
+print(garden_waitlist)
+garden_waitlist[-1] = "Alex"
+print(garden_waitlist)
