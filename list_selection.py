43e09dd268b6f926343639d3bc0c89fa26506e03
jon-xo <jonn@hey.com>
2021-09-02 09:52:19 -0400

    Created list_selection.py to learn about targeting a single item via index

---
+++ list_selection.py
@@ -0,0 +1,11 @@
+# list_selection.py
+## A negative index can be used to select the last element in a list
+
+shopping_list = ["eggs", "butter", "milk", "cucumbers", "juice", "cereal"]
+
+last_element = shopping_list[-1]
+
+index5_element = shopping_list[5]
+
+print(last_element)
+print(index5_element)
