d8279c471a0365339412d6d30dcd52389ceaa5b1
Jon Newton <jonn@hey.com>
2021-09-03 15:08:34 -0400

    Created advanced_lists.py to pratice using python methods and functions for list manipulation, added comments.

---
+++ lists/advanced_lists.py
@@ -0,0 +1,10 @@
+# advanced_lists.py
+## Python list manipulation using built-in methods
+
+## .count() -- List method to count the number of matching occurences in a list
+## .insert() -- List method to insert an element into a specific list index
+## .pop() -- List method to remove an element from a specific index / end of a list
+## range() -- Built-in function to create a sequence of integers
+## len() -- Built-in function to get the length of a list
+## .sort() / .sorted() -- A method & built-in function to sort a list
+
