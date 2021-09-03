0e8172979cc52b1bdcd1a3116c1bd8778e5d883d
Jon Newton <jonn@hey.com>
2021-09-03 15:42:35 -0400

    Added examples in advanced_lists.py for insert and pop methods

--- lists/advanced_lists.py
+++ lists/advanced_lists.py
@@ -8,3 +8,23 @@
 ## len() -- Built-in function to get the length of a list
 ## .sort() / .sorted() -- A method & built-in function to sort a list
 
+## Insert
+## Method takes two inputs, the index you want to insert into
+##  and the element you want to insert into
+
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+front_display_list.insert(0, "Pineapple")
+print(front_display_list)
+
+## Pop
+## Method takes an single (optional) input
+## Once called the original list is modified and the removed value is dropped
+## To retain the removed information, set the output of a method equal to a declared variable
+
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+
+data_science_topics.pop()
+print(data_science_topics)
+data_science_topics.pop(3)
+print(data_science_topics)
