f8f3ee3df214ea6e841c1a2b471c27f951e2d7cc
Jon Newton <jonn@hey.com>
2021-09-07 16:59:41 -0400

    Created for_loops.py with multiple examples

---
+++ loops/for_loops.py
@@ -0,0 +1,16 @@
+# for_loops.py
+
+# for loops allow to perform an action for each iterable element in a collection
+
+ingredients = ["milk", "sugar", "vanilla extract", "dough", "chocolate"]
+
+for ingredient in ingredients:
+    print(ingredient)
+
+# for loops can be combined with range to specify a number of actions with
+# an arbiturary list
+
+for i in range(6):
+    print("Pass",str(i+1),"Loops pratice")
+
+
