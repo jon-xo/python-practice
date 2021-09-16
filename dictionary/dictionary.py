490321724fa22f3a731c68dadc53c3fea37611d2
Jon Newton <jon-xo@users.noreply.github.com>
2021-09-15 21:50:39 -0400

    Updated dictionary/dictionary.py
    Added more examples on adding and modifying key/value pairs

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -4,3 +4,29 @@
 
 menu = {"avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
 print(menu)
+
+# keys and values can support multiple types
+# values can include other dictionaries
+
+students_in_classes = {"software design": ["Aaron", "Delila", "Samson"], "cartography": ["Christopher", "Juan", "Marco"], "philosophy": ["Frederica", "Manuel"]}
+
+translation = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "rock"}
+
+# keys can not contain lists, dictionaries keys are typically string or numbers, which allow for specific hash values
+
+children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+# Add single [keys: value]
+# If the key already exists in the dictionary, this will update the value
+
+menu["cheesecake"] = 8
+
+print(menu)
+
+# Add multiple [keys: value] with the update method
+# update accepts one or more key value pairs in the form of a dictionary
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print()
+
--- strings/string_methods.py
+++ strings/string_methods.py
@@ -51,7 +51,7 @@ reapers_line_one = ' '.join(reapers_line_one_words)
 
 winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
 
-winter_trees_full = '\n'.join(winter_trees_lines)
+winter_trees_full = '\n'.join(win59ter_trees_lines)
 print(winter_trees_full)
 
 ## .replace()
