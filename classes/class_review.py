b75f907effcb2f8ec77eb337e4ccaa97e5bd7f31
Jon Newton <jnewton@mountainleverage.com>
2021-10-14 09:56:15 -0400

    Updated class_review.py with Grade challenge

--- classes/class_review.py
+++ classes/class_review.py
@@ -1,5 +1,17 @@
 # class_review.py
 
+class Grade:
+    minimum_passing = 65
+
+    def __init__(self, score):
+        self.score = score
+
+    def is_passing(self):
+        if self.score > Grade.minimum_passing:
+            return True
+        else:
+            return False
+
 class Student:
     def __init__(self, name, year):
         self.name = name
@@ -15,6 +27,11 @@ class Student:
         if type(grade) == Grade:
             self.grades.append(grade)
 
+    def get_average(self):
+        
+
+            
+
 # Create three instances of the Student class:
 
 # Roger van der Weyden, year 10
@@ -24,18 +41,22 @@ roger = Student("Roger van der Weyden", 10)
 sandro = Student("Sandro Botticelli", 12)
 pieter = Student("Pieter Bruegel the Elder", 8)
 
-
-class Grade:
-    minimum_passing = 65
-
-    def __init__(self, score):
-        self.score = score
-
-
 pieter.add_grade(Grade(100))
+
 
 # Challenges
 
 # Write a Grade method .is_passing() that returns whether a Grade has a passing .score.
-# Write a Student method get_average() that returns the student’s average score.
+
+my_grade = Grade(75)
+attempt_grade = Grade(55)
+
+print(my_grade.is_passing())
+print(attempt_grade.is_passing())
+
+# Write a Student method get_average() that returns the student’s average score
+
+
+
 # Add an instance variable to Student that is a dictionary called .attendance, with dates as keys and booleans as values that indicate whether the student attended school that day.
+
